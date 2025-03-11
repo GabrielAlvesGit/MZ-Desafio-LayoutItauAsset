6cdd0d237228fd03b0ffb15de3630c732870f4b0
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-03-11 15:32:08 -0300

    click end

--- js/banner.js
+++ js/banner.js
@@ -1,15 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var dots = document.querySelectorAll('.js-dot');
-  var slicks = document.querySelectorAll('.js-active-slick');
-  dots[0].classList.add('active');
-  slicks[0].classList.add('active');
+  function slick() {
+    var dots = document.querySelectorAll('.js-dot');
+    var slicks = document.querySelectorAll('.js-active-slick');
+    var slicksTitles = document.querySelectorAll('.js-active-title');
+    var slicksLinks = document.querySelectorAll('.js-active-link');
+    var ClassActive = 'active';
 
-  dots.forEach((dot, slicks) => {
-    dot.addEventListener('click', (content) => {
-      dots.forEach(dot => dot.classList.remove('active'));
-      dot.classList.add('active');     
-      content.classList.add('active');     
-      slicks.classList.add('active');     
-    }); 
-  })  
+    // active
+    dots[0].classList.add(ClassActive);
+    slicks[0].classList.add(ClassActive);
+    slicksTitles[0].classList.add(ClassActive);
+    slicksLinks[0].classList.add(ClassActive);
+
+    dots.forEach((dot, index) => {
+      dot.addEventListener('click', () => {
+        // Dots Slick
+        dots.forEach(dot => dot.classList.remove(ClassActive));
+        dot.classList.add(ClassActive);      
+        
+        // IMG Slick
+        slicks.forEach(slick => slick.classList.remove(ClassActive));
+        slicks[index].classList.add(ClassActive);
+
+        // Title Slick
+        slicksTitles.forEach(slickTitle => slickTitle.classList.remove(ClassActive));
+        slicksTitles[index].classList.add(ClassActive);
+
+        // Link Slick
+        slicksLinks.forEach(slickLink => slickLink.classList.remove(ClassActive));
+        slicksLinks[index].classList.add(ClassActive);
+      }); 
+    })  
+  } 
+
+  slick();
 });
